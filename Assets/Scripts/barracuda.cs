2b4b20c808ca63ba47604d9eb74db6aa97e73eda
pramesh025 <076bei025.pramesh@pcampus.edu.np>
2023-02-21 12:42:03 +0545

    nothhing much

---
+++ Assets/Scripts/barracuda.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barracuda : MonoBehaviour
+{
+    // public NNModel modelAsset;
+    // private Model m_RuntimeModel;
+    // var worker = WorkerFactory.CreateWorker(< WorkerFactory.Type >, m_RuntimeModel);
+    void Start()
+    {
+        // m_RuntimeModel = ModelLoader.Load(modelAsset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
